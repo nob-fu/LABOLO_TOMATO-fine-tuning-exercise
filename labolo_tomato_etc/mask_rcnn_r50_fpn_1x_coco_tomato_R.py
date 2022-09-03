1ff610b7369892869130a3d0076e85578e4f912f
FUJIWARA, Noboru <66042826+nob-fu@users.noreply.github.com>
2022-09-03 16:49:51 +0900

    materials upload

---
+++ labolo_tomato_etc/mask_rcnn_r50_fpn_1x_coco_tomato_R.py
@@ -0,0 +1,6 @@
+# replace mmdetection/configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py 
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/laboro_tomato_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
